01b48463c0fdff7cec17f2be2e0b8450898f6023
Ian Limasi <ianlimasi@icloud.com>
2026-06-06 17:50:46 -0700

    Added Bushes

--- sketch.js
+++ sketch.js
@@ -55,7 +55,8 @@ let hands = [];
 let _shearsR;
 let _shearsL;
 
-let osc;
+// let osc;
+let cutSound;
 
 let clouds = [];
 
@@ -64,7 +65,10 @@ let shearsClosedImg;
 let invasiveImgs = {};
 let nativeImgs = {};
 
+let bushes = [];
+
 function preload() {
+  cutSound = loadSound('sounds/cut.mp3');
   invasiveImgs['blackberry'] =
     [loadImage('images/Invasive_Blackberry.png'), 
      loadImage('images/Invasive_Blackberry2.png')
@@ -90,6 +94,9 @@ function preload() {
     loadImage('images/Native_Camas.png'),
     loadImage('images/Native_Camas2.png')
   ];
+
+  bushes.push(loadImage('images/Bush.svg'));
+  bushes.push(loadImage('images/Bush2.svg'));
 
   shearsImg = loadImage('images/ShearsOpen.png');
   shearsClosedImg = loadImage('images/ShearsClosed.png');
@@ -149,9 +156,9 @@ function draw() {
   drawClouds();
 
   noTint();
-  stroke("#62B46A");
+  stroke("#5AA553");
   strokeWeight(10);
-  fill("#8CCA72");
+  fill("#84C25A");
   let hillRise = _landStart * 0.1;
   beginShape();
   vertex(0, height);
@@ -168,6 +175,15 @@ function draw() {
   } else if (_gameState == GAME_OVER) {
     gameOver();
   }
+
+  let i = floor(frameCount / 20) % bushes.length;
+  image(bushes[i], 0, _landStart/1.2, bushes[i].width / 2.4, bushes[i].height / 2.4);
+  scale(-1, 1);
+  image(bushes[i], -width, _landStart/1.2, bushes[i].width / 2.4, bushes[i].height / 2.4);
+  scale(-1, 1);
+
+  updateShears();
+  drawShears();
 }
 
 function gameMenu() {
@@ -204,9 +220,6 @@ function gamePlay() {
   
   garden.draw();
   drawScore();
-  
-  updateShears();
-  drawShears();
   
   if (garden.getHealth() == 0) {
     _gameState = GAME_OVER;
@@ -294,6 +307,7 @@ function mouseClicked() {
   // } 
   // else {
   //   _shearsL.setCut();
+  //   shearsCut(_shearsL, _invasiveL);
   //   playCutSound();
   // }
   
@@ -338,10 +352,12 @@ async function connectBLE() {
 }
 
 function playCutSound() {
-  osc.start();
-  osc.freq(800);
-  osc.amp(0.5, 0.01);
-  osc.amp(0, 0.1, 0.05);
+  // osc.start();
+  // osc.freq(800);
+  // osc.amp(0.5, 0.01);
+  // osc.amp(0, 0.1, 0.05);
+
+  cutSound.play(0, 1, 1, 1.5, 1);
 }
 
 function createClouds() {
